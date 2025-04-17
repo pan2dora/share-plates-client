cf46bff9693713ee06ec878ddbf7755c9287a459
Pandora <pan2dora.brown@gmail.com>
2025-04-16 20:44:00 -0400

    Created data.js file and added state to handle the data in the home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,72 +1,35 @@
+import recipesData from "../data/recipes";
+import { useState, useEffect } from "react";
+
 function Home() {
-    return (
-      <>
-  {/* FIRST CARD */}
-  <div className="card-container">
-    {/* Still deciding if I want entire card to be link */}
-    <a href="update.html">
-      <article className="card">
-        <img src="public/images/placeholder.png" alt="test" />
-        <div className="card-content">
-          <h1 className="card-title">Recipe</h1>
-          <p className="card-description">Blerb goes header</p>
-        </div>
-      </article>
-    </a>
-    {/* SECOND CARD */}
-    <a href="#">
-      <article className="card">
-        <img src="public/images/placeholder.png" alt="test" />
-        <div className="card-content">
-          <h1 className="card-title">Recipe</h1>
-          <p className="card-description">Blerb goes header</p>
-        </div>
-      </article>
-    </a>
-    {/* THIRD CARD*/}
-    <a href="#">
-      <article className="card">
-        <img src="public/images/placeholder.png" alt="test" />
-        <div className="card-content">
-          <h1 className="card-title">Recipe</h1>
-          <p className="card-description">Blerb goes header</p>
-        </div>
-      </article>
-    </a>
-    {/* FOURTH CARD */}
-    <a href="#">
-      <article className="card">
-        <img src="public/images/placeholder.png" alt="test" />
-        <div className="card-content">
-          <h1 className="card-title">Recipe</h1>
-          <p className="card-description">Blerb goes header</p>
-        </div>
-      </article>
-    </a>
-    {/* FIFTH CARD */}
-    <a href="#">
-      <article className="card">
-        <img src="public/images/placeholder.png" alt="test" />
-        <div className="card-content">
-          <h1 className="card-title">Recipe</h1>
-          <p className="card-description">Blerb goes header</p>
-        </div>
-      </article>
-    </a>
-    {/* SIXTH CARD */}
-    <a href="#">
-      <article className="card">
-        <img src="public/images/placeholder.png" alt="test" />
-        <div className="card-content">
-          <h1 className="card-title">Recipe</h1>
-          <p className="card-description">Blerb goes header</p>
-        </div>
-      </article>
-    </a>
-  </div>
-</>
+  //Use state to handle recipes data
 
-    )
-  }
+  const [recipes, setRecipes] = useState([]);
 
-  export default Home;
+  useEffect(() => {
+    setRecipes(recipesData);
+    console.log("Use effect works");
+  }, []);
+
+  return (
+    <>
+      {/* FIRST CARD */}
+      {recipes.map((recipe) => (
+        <div className="card-container" key={recipe.id}>
+          {/* Still deciding if I want entire card to be link */}
+          <a href="#">
+            <article className="card">
+              <img src="public/images/placeholder.png" alt="test" />
+              <div className="card-content">
+                <h1 className="card-title">{recipe.title}</h1>
+                <p className="card-description">{recipe.blerb}</p>
+              </div>
+            </article>
+          </a>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default Home;
