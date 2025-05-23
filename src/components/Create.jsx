59c69eca0033f41e0d7cca261d3e5fe1a5205965
Pandora <pan2dora.brown@gmail.com>
2025-05-22 21:03:19 -0400

    Added drop down to nav bar

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,23 +1,56 @@
 import { useState } from "react";
 
 function Create() {
+
+
+  // handles the form data
   const [formData, setFormData] = useState({
     recipe: "",
     image: "",
     about: "",
-    items: "",
+    items: [],
     price: "",
-    instructions: "",
+    instructions: [],
     diet: "",
   });
+// Handle submit
 
+const handleSubmit = (e) => {
+  e.preventDefault(console.log("Submitted"));
+  const body = {
+  recipe: e.target.recipe.value,
+  };
+}
+
+
+
+
+  // This state sets the id for items since everyone must be unique
+  const [count, setCount] = useState(0);
+  // does the same thing for instructions
+  const [countInstructions, setCountInstructions] = useState(0)
+  // Working on logic for this image state that will eventually render image previews
+  // const [image, setImage] = useState("")
+  // These two states handle the ingredient list
   const [newItem, setNewItem] = useState("");
   const [items, setItems] = useState([]);
+  // These two states handle the Instructions list
+  const [newInstruction, setNewInstruction] = useState("");
+  const [instructions, setInstructions] = useState([]);
 
-  const handleSubmit = (e) => {
+  const handleListSubmit = (e) => {
     e.preventDefault();
+    setCount((prevCount) => prevCount + 1);
     setItems((prevItems) => {
-      return [...prevItems, { id: 1, title: newItem }];
+      return [...prevItems, { id: count + 1, title: newItem }];
+    });
+  };
+
+  const handleInstructionsubmit = (e) => {
+    e.preventDefault();
+    setCountInstructions((prevCount) => prevCount + 1);
+    setInstructions((prevItems) => {
+      return [...prevItems, { id: countInstructions + 1, title: newInstruction }];
     });
   };
 
@@ -28,38 +61,51 @@ function Create() {
     console.log(value);
     setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
   };
+  // Create Image preview
 
-  console.log(items);
-
-  // console.log(formData);
+  console.log("Items:", items);
+console.log("Instructions:", instructions)
+  console.log("Form Data:", formData);
 
   return (
+
     <div className="form-wrapper">
-      <form className="create-form">
+      <div className="create-form-header">
+        <h1>Add Recipe</h1>
+      </div>
+    <div className="create-form-blerb">
+    <h2>Add your favorite recipe and share it with community!</h2>
+    </div>
+      <form onSubmit={handleSubmit} className="create-form" >
         {/* RECIPE NAME */}
         <div className="flex-input">
-          <label htmlFor="recipe">Recipe</label>
+          <label htmlFor="recipe">Recipe Name</label>
           <input
             type="text"
             name="recipe"
             id="recipe"
+            placeholder="What's your recipes name?"
             onChange={handleChange}
           />
         </div>
         {/* IMAGE UPLOAD */}
+        {/* I want the image to display as a preview */}
         <div className="flex-input">
           <label htmlFor="image">Image</label>
           <input type="file" name="image" id="image" onChange={handleChange} />
         </div>
+        <div>
+          <img src="{image}" alt="preview image" />
+        </div>
         {/* BLERB */}
         <div className="flex-input">
-          <label htmlFor="about">About Recipe</label>
-          <input type="text" name="about" id="about"  onChange={handleChange}/>
+          <label htmlFor="about">Description</label>
+          <input type="text" name="about" id="about" onChange={handleChange} placeholder="Tell us a story about your recipe" />
         </div>
         {/* SHOPPING LIST */}
         {/* Use state to update this  */}
         <div className="flex-input">
-          <label htmlFor="items">Shopping List</label>
+          <label htmlFor="items">Ingredients</label>
           <input
             value={newItem}
             type="text"
@@ -68,31 +114,54 @@ function Create() {
             onChange={(e) => setNewItem(e.target.value)}
           />
 
-          <button onClick={handleSubmit} className="items-btn">
+          <button onClick={handleListSubmit} className="items-btn">
             +
           </button>
           {/* Figure out ui for  */}
           <ul className="shopping-list">
             {items.map((item) => {
-              return <li>{item.title}</li>;
+              return <li key={item.id}>{item.title}</li>;
+            })}
+          </ul>
+          <button className="btn-delete">-</button>
+        </div>
+{/* This will eventually be connected to the ingredients but will work on this in the backend because I need to add all the values */}
+        <div className="flex-input">
+          <label htmlFor="price">Price</label>
+          <input
+            type="number"
+            name="price"
+            id="price"
+            onChange={handleChange}
+          />
+        </div>
+        {/* INSYRUCTIONS */}
+        <div className="flex-input">
+          <label htmlFor="instructions">Instructions</label>
+          <input
+            value={newInstruction}
+            type="text"
+            name="instructions"
+            id="instructions"
+            onChange={(e)=>setNewInstruction(e.target.value)}
+          />
+
+          <button onClick={handleInstructionsubmit} className="items-btn">
+            +
+          </button>
+          {/* Figure out ui for  */}
+          <ul className="instructions">
+            {instructions.map((ingredient) => {
+              return <li key={ingredient.id}>{ingredient.title}</li>;
             })}
           </ul>
           <button className="btn-delete">-</button>
         </div>
 
-        <div className="flex-input">
-          <label htmlFor="price">Price</label>
-          <input type="number" name="price" id="price" />
-        </div>
-        {/* INSYRUCTIONS */}
-        <div className="flex-input">
-          <label htmlFor="">Instructions</label>
-          <input type="text" name="instructions" id="instructions" />
-        </div>
         {/* FILTERS */}
         <div className="flex-input">
-          <label htmlFor="diet">Diet</label>
-          <input type="text" name="diet" id="diet" />
+          <label htmlFor="diet">Tags</label>
+          <input type="text" name="diet" id="diet" onChange={handleChange} />
         </div>
         {/* Eventually this will be where people can input various dietary tags */}
         {/* SUBMIT BUTTON */}
--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,49 +1,64 @@
 function Header() {
-    return (
-<>
-{/* <!-- Header & Primary Nav container --> */}
-    <div className="nav-wrapper">
-      <header>
-        {/* <!--  Hambuger --> */}
-        <a href="#"><i className="fa-solid fa-bars"></i></a>
-        {/* <!-----------------------------Logo ------------------------------------------> */}
-        <img
-          src="public/images/Share.svg"
-          alt="Animation of plate with share plate in the middle"
-          className="nav-logo"
-        />
-        {/* <!-- Start of Primary Navbar --> */}
+  return (
+    <>
+      {/* <!-- Header & Primary Nav container --> */}
+      <div className="navbar-container">
+        <header>
+          {/* <!--  Hambuger --> */}
+          <a href="#">
+            <i className="fa-solid fa-bars"></i>
+          </a>
+          {/* <!-----------------------------Logo ------------------------------------------> */}
+          <img
+            src="public/images/Share.svg"
+            alt="Animation of plate with share plate in the middle"
+            className="nav-logo"
+          />
+          {/* <!-- Start of Primary Navbar --> */}
 
-        <nav className="primary-navigation">
-          {/* <!-----------------------------Navbar Links------------------------------------------> */}
-          <ul className="nav-links">
-            <li><a href="discover.html">Discover</a></li>
-            <li><a href="about.html">About</a></li>
-          </ul>
-        </nav>
+          <nav className="primary-navigation">
+            {/* <!-----------------------------Navbar Links------------------------------------------> */}
+            <ul className="nav-links">
+              <li>
+                <a href="#">Login</a>
+              </li>
+              <li class="dropdown">
+                <a href="#" class="dropbtn">
+                 Profile
+                </a>
+                <div class="dropdown-content">
+                  <a href="#">Profile</a>
+                  <a href="#">Create Recipe</a>
+                  <a href="#">Fav Recipes</a>
+                </div>
+              </li>
+              <li>
+                <a href="discover.html">Discover</a>
+              </li>
+              <li>
+                <a href="about.html">About</a>
+              </li>
+            </ul>
+          </nav>
 
-        {/* <!-----------------------------Search ------------------------------------------> */}
+          {/* <!-----------------------------Search ------------------------------------------> */}
 
-        <div className="search">
-          <input type="text" className="search-bar" placeholder="Search..." />
-          {/* <!-- Search Icon --> */}
-          <i className="fa-solid fa-magnifying-glass"></i>
-        </div>
+          <div className="search">
+            <input type="text" className="search-bar" placeholder="Search..." />
+            {/* <!-- Search Icon --> */}
+            <i className="fa-solid fa-magnifying-glass"></i>
+          </div>
 
-        {/* <!-- Profile Icon - This will eventually become a drop down--> */}
-    <div>
-           <a href="admin.html"><i className="fa-solid fa-circle-user"></i></a>
-        </div>
-      </header>
-    </div>
+          {/* <!-- Profile Icon - This will eventually become a drop down--> */}
+          <div>
+            <a href="admin.html">
+              <i className="fa-solid fa-circle-user"></i>
+            </a>
+          </div>
+        </header>
+      </div>
+    </>
+  );
+}
 
-
-
-
-</>
-
-
-    )
-  }
-
-  export default Header;
+export default Header;
