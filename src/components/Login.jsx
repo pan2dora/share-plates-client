a2e4f91d05c1dc9e03e6f8687b7cd0d70ca33e6f
Pandora <pan2dora.brown@gmail.com>
2025-06-22 15:10:10 -0400

    Fixed signup and login bugs but login does not work

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,52 @@
-function Login() {
 
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+function Login({ setUser }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+  const url = import.meta.env.VITE_API_BASE_URL;
+  const endpoint = "/api/auth/login/local";
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const body = {
+      // email: e.target.email.value,
+      username: e.target.username.value,
+      password: e.target.password.value,
+    };
+
+console.log(body)
+
+
+    setIsLoading(true);
+
+    fetch(`${url}${endpoint}`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(body),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+          console.log("Full result:", result); // This will show you the actual structure
+  console.log("result.data:", result.data);
+  console.log("result.data.newUser:", result.data?.newUser);
+        localStorage.setItem("user", JSON.stringify(result.data.newUser));
+        setUser(result.data.newUser);
+        navigate("../admin");
+      })
+      .catch((error) => setError(error.message))
+      .finally(() => setIsLoading(false));
+  };
 
   
-    return (
-      <div className="form-wrapper">
+  return (
+    <div className="form-wrapper">
       <img
         className="footer-logo"
         src="public/images/sp-logo.svg"
@@ -11,36 +54,39 @@ function Login() {
       />
       <div>
         {/*------------------------Login Form-----------------------------------*/}
-        <form className="login-form">
+        <form onSubmit={handleLogin} className="login-form">
           <div className="flex-input">
-            <label htmlFor="username" /> Username
-            <input type="text" placeholder="Username" />
+            <label htmlFor="username" />Username
+            <input
+              type="text"
+              placeholder="username"
+              id="username"
+              name="username"
+              required
+            />
           </div>
           <div className="flex-input">
             <label htmlFor="password" /> Password
-            <input type="text" placeholder="Password" />
+            <input
+              type="password"
+              placeholder="Password"
+              id="password"
+              name="password"
+            />
           </div>
           {/* using index.html as a placeholder for the button */}
-          <input
-            className="flex"
-            type="button"
-            defaultValue="Login"
-            onclick="window.location.href='index.html'"
-          />
+          <button type="submit" disabled={isLoading}>
+            {isLoading ? "Logining in ..." : "Login"}
+          </button>
+          
+          {error && <p>There is an error: {error}</p>}
         </form>
-        <div className="login-checkbox">
-          <div>
-            <input type="checkbox" name="" id="" />
-            <span>Remember me</span>
-          </div>
-          <div>
-            <a href="#">Forgot Password</a>
-          </div>
+        <div>
+        <p> No account please <Link to="/signup">register</Link> </p>
         </div>
       </div>
-    </div>
-    
-    )
-  }
+    </div> 
+  );
+}
 
-  export default Login;
+export default Login;
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,33 +1,78 @@
-function Signup() {
-     
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-    return (
-      <form className="signup-form">
+function Signup({ user , setUser}) {
+   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+    const navigate = useNavigate(); 
+  const url = import.meta.env.VITE_API_BASE_URL
+  const endpoint = "/api/auth/register"
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    
+    const body = {
+      firstname: e.target.firstname.value,
+      email: e.target.email.value,
+      username: e.target.username.value,
+      password: e.target.password.value,
+    };
+
+    console.log("body", body)
+  setLoading(true)
+
+fetch(`${url}${endpoint}`,{
+  method: "POST",
+  headers: {
+    "Content-type": "application/json",
+  },
+  body: JSON.stringify(body),
+})
+    .then((response) => response.json())
+      .then((result) => {
+    
+        localStorage.setItem("user", JSON.stringify(result.data.newUser));
+        setUser(result.data.newUser);
+        navigate("../admin");
+    
+      })
+      .catch((error) => setError(error.message))
+      .finally(() => setLoading(false));
+  }
+
+
+  return (
+    <form className="signup-form" onSubmit={handleSignup}>
+       {/* First Name */}
+      <div className="flex-input">
+        <label htmlFor="firstname" >First name</label>
+        <input type="text" id="firstname" name="firstname" required />
+      </div>
       {/* USERNAME */}
       <div className="flex-input">
-        <label htmlFor="username">Username</label>
-        <input type="text" />
+        <label htmlFor="username" >Username</label>
+        <input type="text" id="username" name="username" required />
       </div>
       {/* EMAIL */}
       <div className="flex-input">
         <label htmlFor="email">Email</label>
-        <input type="text" />
+        <input type="email" id="email" name="email" />
       </div>
       {/* PASSWORD */}
       <div className="flex-input">
         <label htmlFor="password">Password</label>
-        <input type="text" />
+        <input type="password" id="password" name="password" />
       </div>
-      {/* IMAGE UPLOAD */}
+      {/* IMAGE UPLOAD
       <div className="flex-input">
         <label htmlFor="image">Profile Photo</label>
         <input type="file" />
-      </div>
+      </div> */}
       {/* SIGNUP BUTTON */}
-      <input type="submit" defaultValue="Signup" />
+      <button type="submit">{loading? "Registering" : "Register" }</button>
+      {error && <p>There is an error: {error}</p>}
     </form>
-    
-    )
-  }
+  );
+}
 
-  export default Signup;
+export default Signup;
