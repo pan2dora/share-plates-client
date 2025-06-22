2011a279349e7ca6aa52ab5681c23f0601309beb
Pandora <pan2dora.brown@gmail.com>
2025-06-22 15:22:09 -0400

    Added styling to admin page

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from "react";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import { Link } from "react-router-dom";
+
 function Admin() {
+const [recipes, setRecipes] = useState([]);
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+  const endpoint = "/api/recipes";
+  //Random assortment of recipes, later will be randomize based on users filters
+  useEffect(() => {
+    fetch(`${API_BASE_URL}${endpoint}`, {
+      method: "GET",
+      headers: { "Content-type": "application/json" },
+      credintials: "included",
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log("Success:", result.success.message);
+        setRecipes(result.data.recipes);
+      })
+      .catch(console.log("error:"));
+  }, [API_BASE_URL]);
+  console.log("API URL", );
+  const endpoint2 = "/api/recipe/";
+  const url = `${API_BASE_URL}${endpoint2}`;
+  console.log("url", `${url}`);
+  console.log("Recipe object", recipes);
+  console.log("recipe id", recipes._id);
+
+ 
+  
     return (
+     <>
       <form className="signup-form">
       {/* USERNAME */}
       <div className="flex-input">
@@ -24,8 +57,39 @@ function Admin() {
       {/* SIGNUP BUTTON */}
       <input type="submit" defaultValue="Save" />
     </form>
+<div className="card-wrapper">
+   <h2>Your recipes</h2>
+   
+
+        {recipes.map((recipe) => (
+          <Row key={recipe._id}>
+          <Col  >
+       <Card col-lg-2 className="card">  
+            
+            <Card.Img
+              className="card-image"
+              src={recipe.image}
+              alt="Image of food"
+            />
+
+            <Card.Body className="card-content">
+              <Link to={`/recipe/${recipe._id}`}>
+                <Card.Title>{recipe.recipe}</Card.Title>
+              </Link>
+              <Card.Text>{recipe.about}</Card.Text>
+            </Card.Body>
+            </Card>
+          </Col></Row>
+        ))}
+       
+ 
+   </div>
+  
+   
+</>
     
     )
+
   }
 
   export default Admin;
